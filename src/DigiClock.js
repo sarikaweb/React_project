b586b27e61dcad878466e9c55d21775641489a86
Sarika <sarika.web03@gmail.com>
2022-05-04 10:58:01 +0530

    clock update here

--- src/DigiClock.js
+++ src/DigiClock.js
@@ -1,15 +1,10 @@
-import React, { Component, useState } from 'react';
-
-
-
-
+import React, { useState } from 'react';
 class DigiClock extends React.Component {
     constructor(props) {
         super(props);
-  
-    console.log('state here...');
     this.state = {
-        time: new Date().toLocaleString()
+        time: new Date().toLocaleString(),
+        delete: false,
     };
 }
 static getDerivedStateFromProps(props, state) {
@@ -17,22 +12,38 @@ static getDerivedStateFromProps(props, state) {
     return null;
 }
 componentDidMount() {
-    setInterval(() => {
-          this.setState({
-              time: new Date().toLocaleString(),
-          });
-      }, 1000);
-  }
+    console.log('mount here...');
+      this.timerID = setInterval(
+        () => this.clockTime(),
+        1000
+      );
+    }
+  
+  
 componentWillUnmount(){
-    
+console.log('unmount here...');
+clearInterval(this.timerID);
 }
+
+ clockTime() {
+    this.setState({
+       time: new Date().toLocaleString(),
+   });
+  
+  }
+
+
+
 render() {
+    
     return (
         <>
          <div className='date-time'>
              <h1>Digital Clock</h1>
 
              <h6>Date & Time : {this.state.time}</h6>
+
+           
          </div>
       </>
     )
